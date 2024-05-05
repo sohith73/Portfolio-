7670123d16fcd8bbaa153f7e7e6a6852dbcbbd77
Sohith Bandreddi <sohith73@gmail.com>
2024-05-05 07:03:12 +0530

    cloud links

--- src/components/ProjectsMain.jsx
+++ src/components/ProjectsMain.jsx
@@ -18,51 +18,51 @@ const ProjectsMain = () => {
                <motion.div className='flex-row gap-8'>
                <motion.div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
                     <Link to='/'><ProjectCard
-                         src={img3}
+                         src='https://res.cloudinary.com/dcqslfpad/image/upload/v1714459346/samples/portfolio/wrjimjt08f5volx66fbw.png'
                          title="Modern Next.js Portfolio"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     /></Link>
                     <Link to='/'><ProjectCard
-                         src={img2}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Interactive Website Cards"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     /></Link>
                     <Link to='/'><ProjectCard
-                         src={img1}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Space Themed Website"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     /></Link>
                </motion.div>
                <motion.div className="h-full mt-10 w-full flex flex-col md:flex-row gap-10 px-10">
                     <ProjectCard
-                         src={img3}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459346/samples/portfolio/wrjimjt08f5volx66fbw.png"
                          title="Modern Next.js Portfolio"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
                     <ProjectCard
-                         src={img2}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Interactive Website Cards"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
                     <ProjectCard
-                         src={img1}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Space Themed Website"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
                </motion.div>
                <motion.div className="h-full mt-10 w-full flex flex-col md:flex-row gap-10 px-10">
                     <ProjectCard
-                         src={img3}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459346/samples/portfolio/wrjimjt08f5volx66fbw.png"
                          title="Modern Next.js Portfolio"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
                     <ProjectCard
-                         src={img2}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Interactive Website Cards"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
                     <ProjectCard
-                         src={img1}
+                         src="https://res.cloudinary.com/dcqslfpad/image/upload/v1714459549/samples/portfolio/iglmkg84oqhmx3tfkkyp.png"
                          title="Space Themed Website"
                          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
                     />
